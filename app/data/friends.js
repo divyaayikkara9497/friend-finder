80d3990808bed7c98d49a2ad6ee1896905a19243
Divya Ayikkara <divyaayikkara@nwk--200-185.nwk.ruw.rutgers.edu>
2018-02-05 16:17:01 -0500

    html pages done

---
+++ app/data/friends.js
@@ -0,0 +1,155 @@
+var friendsList = [
+{
+	"name" : "Sarah",
+	"photo" : "http://cutepuppydog.net/wallpapers/cute-puppy11.jpg",
+	"score": [
+	5,
+    1,
+    4,
+    4,
+    5,
+    1,
+    2,
+    5,
+    4,
+    1
+    ]
+}, {
+	"name" : "Maya",
+	"photo" : "https://i.pinimg.com/736x/02/d7/01/02d701b77a984a1b6cf970e6eb0468e1--teacup-maltipoo-maltipoo-puppies.jpg",
+	"score" : [
+	1,
+	1,
+	1,
+	1,
+	1,
+	1,
+	1,
+	1,
+	1,
+	1
+	]
+} , {
+	"name" : "Kelsey",
+	"photo": "http://doggiemanners.com/wp-content/uploads/2015/02/money-teacup-wp.jpg",
+	"score" : [
+	2,
+	2,
+	2,
+	2,
+	2,
+	2,
+	2,
+	2,
+	2,
+	2
+	]
+} , {
+	"name" : "Ana",
+	"photo" : "http://images6.fanpop.com/image/photos/34200000/Puppy-dogs-34212464-1440-900.jpg",
+	"score" : [
+	3,
+	3,
+	3,
+	3,
+	3,
+	3,
+	3,
+	3,
+	3,
+	3
+	]
+} , {
+	"name" : "Jack",
+	"photo" : "http://cdn.attackofthecute.com/October-30-2012-02-53-56-aa.jpeg",
+	"score" : [
+	4,
+	4,
+	4,
+	4,
+	4,
+	4,
+	4,
+	4,
+	4,
+	4
+	]
+} , {
+	"name" : "Tess",
+	"photo" : "https://i.ytimg.com/vi/qryK_mfuZV0/maxresdefault.jpg",
+	"score" : [
+	5,
+	5,
+	5,
+	5,
+	5,
+	5,
+	5,
+	5,
+	5,
+	5
+	]
+} , {
+	"name" : "Randall",
+	"photo" : "https://media4.s-nbcnews.com/i/newscms/2016_34/1152399/today-show-puppy-007-tease-today-160822_752fa6dc6e57444c771d278a157ed7c9.jpg",
+	"score" : [
+	4,
+	2,
+	2,
+	2,
+	4,
+	4,
+	2,
+	2,
+	2,
+	4
+	]
+} , {
+	"name" : "Rebacca",
+	"photo" : "http://greatinspire.com/wp-content/uploads/2012/11/Cute-puppy-photos-422.jpg",
+	"score" : [
+	3,
+	1,
+	5,
+	5,
+	3,
+	1,
+	5,
+	3,
+	3,
+	1
+	]
+} , {
+	"name" : "Kevin",
+	"photo" : "https://images-na.ssl-images-amazon.com/images/I/71cuOAWZ3PL._SL1000_.jpg",
+	"score" : [
+	2,
+	2,
+	4,
+	2,
+	4,
+	2,
+	4,
+	4,
+	4,
+	4
+	]
+} , {
+	"name" : "Tony",
+	"photo" : "https://i.pinimg.com/236x/dd/08/11/dd0811fc907c99e10c9b9c5f929f5c2c--real-estate-investing-grumpy-dog.jpg",
+	"score" : [
+	3,
+	1,
+	1,
+	3,
+	5,
+	1,
+	5,
+	3,
+	1,
+	3
+	]
+}
+];
+
+module.exports = friendsList;
